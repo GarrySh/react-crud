5a84dcfd1a392678fcf6f980a632554215963dde
GarrySh <sooo.lame@gmail.com>
2018-11-02 17:53:46 +1000

    add components

---
+++ src/app.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Root from './components/Root';
+
+export default () => {
+  ReactDOM.render(<Root />, document.getElementById('root'));
+};
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => (
+  <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a className="navbar-brand" href="/">
+      Navbar
+    </a>
+  </nav>
+);
+
+export default Header;
