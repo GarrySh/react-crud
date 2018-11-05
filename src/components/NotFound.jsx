3e64dedde9be9698157c88f9b573a857317d308a
GarrySh <sooo.lame@gmail.com>
2018-11-05 20:45:24 +1000

    add router

---
+++ src/components/NotFound.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const NotFound = () => (
+  <div className="alert alert-warning">
+    <h4 className="alert-heading">Warning!</h4>
+    <p className="mb-0">Page not found</p>
+  </div>
+);
+
+export default NotFound;
--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Header from './Header';
 import LandingPage from './LandingPage';
+import NotFound from './NotFound';
 
 export default () => (
-  <div>
-    <Header />
-    <main className="container">
-      <LandingPage />
-    </main>
-  </div>
+  <Router>
+    <div>
+      <Header />
+      <main className="container">
+        <Switch>
+          <Route path="/" component={LandingPage} exact />
+          <Route path="*" component={NotFound} />
+        </Switch>
+      </main>
+    </div>
+  </Router>
 );
